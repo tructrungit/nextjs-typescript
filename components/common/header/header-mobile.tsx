d26c60c214f75bbfb3f74805d38ef9d6c0905aa0
trucdt87 <trucdt@devblock.net>
2022-10-13 15:19:22 +0700

    add header mobile

--- components/common/header/header-mobile.tsx
+++ components/common/header/header-mobile.tsx
@@ -1,13 +1,32 @@
-import { Box } from '@mui/material';
-import * as React from 'react';
+import { Box, Container, Link as MuiLink, Stack } from '@mui/material';
+import Link from 'next/link';
+import { ROUTE_LIST } from './routes';
+import clsx from "clsx";
+import { useRouter } from 'next/router';
+
 
 export interface HeaderMobileProps {
 }
 
 export function HeaderMobile (props: HeaderMobileProps) {
+  const router = useRouter();
+
   return (
     <Box display={{ xs: 'block', lg: 'none'}}>
-        Header Mobile
+        <Container>
+        <Stack direction="row" justifyContent="space-around">
+          {ROUTE_LIST.map((route) => (
+            <Link key={route.path} href={route.path} passHref>
+              <MuiLink
+                sx={{ ml: 2 }}
+                className={clsx({ active: router.pathname === route.path })}
+              >
+                {route.label}
+              </MuiLink>
+            </Link>
+          ))}
+        </Stack>
+      </Container>
     </Box>
   );
 }
