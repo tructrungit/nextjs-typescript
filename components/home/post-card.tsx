897615d61a4cfad4c95aa7322f76fa976448f4b4
trucdt87 <trucdt@devblock.net>
2022-10-14 14:47:09 +0700

    04-14 create Post Card

--- components/home/post-card.tsx
+++ components/home/post-card.tsx
@@ -1,12 +1,32 @@
 import * as React from "react";
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, Typography, Divider } from "@mui/material";
+import { Post } from "../../models";
+import { format } from 'date-fns'
 
-export interface PostCardProps {}
+export interface PostCardProps {
+  post: Post;
+}
 
-export function PostCard(props: PostCardProps) {
+export function PostCard({ post }: PostCardProps) {
+  //   console.log(post);
+  if (!post) return null;
   return (
     <Card>
-      <CardContent>Post title</CardContent>
+      <CardContent>
+        <Typography variant="h5" fontWeight="bold">
+          {post.title}
+        </Typography>
+
+        <Typography component={'span'} variant="body1" my={2} sx={{ display: "flex" }}>
+          {format(Number(post.publishedDate), 'dd MMM yyyy')}
+
+          <Divider orientation="vertical" flexItem sx={{ mx: 2 }} />
+          
+          {post.tagList.join(", ")}
+        </Typography>
+
+        <Typography variant="body1">{post.description}</Typography>
+      </CardContent>
     </Card>
   );
 }
--- components/home/recent-posts.tsx
+++ components/home/recent-posts.tsx
@@ -1,16 +1,37 @@
 import { Box, Link as MuiLink, Stack, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import Link from "next/link";
+import { Post } from "../../models";
 import { PostCard } from "./post-card";
 
 export function RecentPosts() {
+  //  Call API get recent Post
+  const postList: Post[] = [
+    {
+      id: "1",
+      title: "Making a design system from scratch",
+      publishedDate: "1665717927423",
+      tagList: ["Design", "Pattern"],
+      description:
+        "Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Proin eget tortor risus. Donec sollicitudin molestie malesuada.",
+    },
+    {
+      id: "2",
+      title: "Create pixel perfect icons in Figma",
+      publishedDate: "1665717927423",
+      tagList: ["Figma", "Icon Desgin"],
+      description:
+        "Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.",
+    },
+  ];
+
   return (
     <Box component="section" bgcolor="secondary.light" pt={2} pb={4}>
       <Container>
         <Stack
           direction="row"
           mb={2}
-          justifyContent={{ xs: 'center', md: "space-between"}}
+          justifyContent={{ xs: "center", md: "space-between" }}
           alignContent="center"
         >
           <Typography variant="h5">Recent Posts</Typography>
@@ -36,13 +57,11 @@ export function RecentPosts() {
             },
           }}
         >
-          <Box>
-            <PostCard />
-          </Box>
-
-          <Box>
-            <PostCard />
-          </Box>
+          {postList.map((post) => (
+            <Box key={post.id}>
+              <PostCard post={post} />
+            </Box>
+          ))}
         </Stack>
       </Container>
     </Box>
